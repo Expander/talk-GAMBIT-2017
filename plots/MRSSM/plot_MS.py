676287cbe4e0ea7c2acd51776a53a64a2ee2d757
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-10-24 10:12:13 +0200

    adding MRSSM plot

---
+++ plots/MRSSM/plot_MS.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
+import matplotlib.patches as patches
+import scipy.interpolate
+plt.rcParams['text.usetex'] = True
+
+directory = r'plots/MRSSM/'
+
+# plot Mh(MS)
+
+outfile = directory + r'MRSSM2_MS_amu_Mh_MW_MG.pdf'
+
+try:
+    data = np.genfromtxt(directory + r'scan_MRSSM2_MS_amu_Mh_MW_MG.dat')
+except:
+    print "Error: could not load numerical data from file"
+    exit
+
+MS          = data[:,0]
+amu         = data[:,1] * 10**10
+Mh          = data[:,2]
+MW          = data[:,3]
+MG          = data[:,4]
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', weight='normal')
+fig = plt.figure(figsize=(4,4))
+plt.gcf().subplots_adjust(bottom=0.15, left=0.15) # room for xlabel
+ax = plt.gca()
+ax.set_axisbelow(True)
+# ax.xaxis.set_major_formatter(tck.FormatStrFormatter(r'$%d$'))
+# ax.yaxis.set_major_formatter(tck.FormatStrFormatter(r'$%d$'))
+ax.get_yaxis().set_tick_params(which='both',direction='in',colors='r')
+ax.get_xaxis().set_tick_params(which='both',direction='in')
+plt.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
+# plt.grid(color='0.5', linestyle=':', linewidth=0.2, dashes=(0.5,1.5))
+
+plt.xscale('log')
+plt.xlabel(r'$M_S\,/\,\mathrm{GeV}$')
+plt.ylabel(r'$M_h\,/\,\mathrm{GeV}$',color='r')
+
+plt.plot(MS, Mh, 'r-' , linewidth=1.2)
+# plt.plot(MS, MW, 'b--', linewidth=1.2)
+# plt.plot(MS, MG, 'b:' , linewidth=1.2)
+
+# leg = plt.legend([r'$M_h$'
+#                   # r'$M_W$'
+#                   # r'$M_{\tilde{g}}$'
+#                  ],
+#            loc='center right', fontsize=10, fancybox=None, framealpha=None)
+# leg.get_frame().set_alpha(1.0)
+# leg.get_frame().set_edgecolor('black')
+plt.ylim([80,130])
+plt.title(r'$\lambda_u = \lambda_t = \Lambda_u = \Lambda_d = -0.5, \tan\beta = 10$')
+
+Mhexp = 125.09
+sigma = 0.32
+
+ax.add_patch(
+    patches.Rectangle(
+        (ax.get_xlim()[0], Mhexp - sigma)  , # (x,y)
+        ax.get_xlim()[1] - ax.get_xlim()[0], # width
+        2*sigma                            , # height
+        color='orange', alpha=0.5, zorder=-1
+    )
+)
+
+plt.text(ax.get_xlim()[0] + 1500, Mhexp + sigma + 0.4, r"ATLAS/CMS $\pm1\sigma$", fontsize=8)
+
+### g-2 ###
+
+ax2 = ax.twinx()
+ax2.plot(MS, amu, 'b--', linewidth=1.2)
+ax2.set_ylabel(r'$(g-2)_\mu \times 10^{10}$', color='b')
+ax2.tick_params('y', colors='b',direction='in')
+
+plt.xlim([100,10000])
+
+plt.tight_layout()
+plt.savefig(outfile)
+print "saved plot in ", outfile
+plt.close(fig)
---
+++ plots/MRSSM/scan.m
@@ -0,0 +1,133 @@
+Get["models/MRSSM2/MRSSM2_librarylink.m"];
+
+invalid;
+scaleFactor = 10;
+
+LinearRange[start_, stop_, steps_] :=
+    Table[start + i/steps (stop - start), {i, 0, steps}];
+
+(* generate logarithmically spaced range [start, stop] *)
+LogRange[start_, stop_, steps_] :=
+    Exp /@ Range[Log[start], Log[stop], (Log[stop] - Log[start])/steps];
+
+RunMRSSMBMP1[MS_?NumericQ] :=
+    Module[{handle, spec, obs},
+           handle = FSMRSSM2OpenHandle[
+               fsSettings -> {
+                   precisionGoal -> 1.*^-4,           (* FlexibleSUSY[0] *)
+                   maxIterations -> 10000,            (* FlexibleSUSY[1] *)
+                   solver -> 1,                       (* FlexibleSUSY[2] *)
+                   calculateStandardModelMasses -> 0, (* FlexibleSUSY[3] *)
+                   poleMassLoopOrder -> 2,            (* FlexibleSUSY[4] *)
+                   ewsbLoopOrder -> 2,                (* FlexibleSUSY[5] *)
+                   betaFunctionLoopOrder -> 3,        (* FlexibleSUSY[6] *)
+                   thresholdCorrectionsLoopOrder -> 2,(* FlexibleSUSY[7] *)
+                   higgs2loopCorrectionAtAs -> 1,     (* FlexibleSUSY[8] *)
+                   higgs2loopCorrectionAbAs -> 1,     (* FlexibleSUSY[9] *)
+                   higgs2loopCorrectionAtAt -> 1,     (* FlexibleSUSY[10] *)
+                   higgs2loopCorrectionAtauAtau -> 1, (* FlexibleSUSY[11] *)
+                   forceOutput -> 0,                  (* FlexibleSUSY[12] *)
+                   topPoleQCDCorrections -> 1,        (* FlexibleSUSY[13] *)
+                   betaZeroThreshold -> 1.*^-11,      (* FlexibleSUSY[14] *)
+                   forcePositiveMasses -> 0,          (* FlexibleSUSY[16] *)
+                   poleMassScale -> 0,                (* FlexibleSUSY[17] *)
+                   eftPoleMassScale -> 0,             (* FlexibleSUSY[18] *)
+                   eftMatchingScale -> 0,             (* FlexibleSUSY[19] *)
+                   eftMatchingLoopOrderUp -> 2,       (* FlexibleSUSY[20] *)
+                   eftMatchingLoopOrderDown -> 1,     (* FlexibleSUSY[21] *)
+                   eftHiggsIndex -> 0,                (* FlexibleSUSY[22] *)
+                   calculateBSMMasses -> 1,           (* FlexibleSUSY[23] *)
+                   thresholdCorrections -> 123111321, (* FlexibleSUSY[24] *)
+                   higgs3loopCorrectionRenScheme -> 0,(* FlexibleSUSY[25] *)
+                   higgs3loopCorrectionAtAsAs -> 1,   (* FlexibleSUSY[26] *)
+                   higgs3loopCorrectionAbAsAs -> 1,   (* FlexibleSUSY[27] *)
+                   higgs3loopCorrectionAtAtAs -> 1,   (* FlexibleSUSY[28] *)
+                   higgs3loopCorrectionAtAtAt -> 1,   (* FlexibleSUSY[29] *)
+                   parameterOutputScale -> 0          (* MODSEL[12] *)
+               },
+               fsSMParameters -> {
+                   alphaEmMZ -> 1/127.94,  (* SMINPUTS[1] *)
+                   GF -> 1.1663787 10^-5,  (* SMINPUTS[2] *)
+                   alphaSMZ -> 0.1181,     (* SMINPUTS[3] *)
+                   MZ -> 91.1876,          (* SMINPUTS[4] *)
+                   mbmb -> 4.18,           (* SMINPUTS[5] *)
+                   Mt -> 173.34,           (* SMINPUTS[6] *)
+                   Mtau -> 1.77686,        (* SMINPUTS[7] *)
+                   Mv3 -> 0,               (* SMINPUTS[8] *)
+                   MW -> 80.385,           (* SMINPUTS[9] *)
+                   Me -> 0.000510998902,   (* SMINPUTS[11] *)
+                   Mv1 -> 0,               (* SMINPUTS[12] *)
+                   Mm -> 0.1056583715,     (* SMINPUTS[13] *)
+                   Mv2 -> 0,               (* SMINPUTS[14] *)
+                   md2GeV -> 0.00475,      (* SMINPUTS[21] *)
+                   mu2GeV -> 0.0024,       (* SMINPUTS[22] *)
+                   ms2GeV -> 0.104,        (* SMINPUTS[23] *)
+                   mcmc -> 1.27,           (* SMINPUTS[24] *)
+                   CKMTheta12 -> 0,
+                   CKMTheta13 -> 0,
+                   CKMTheta23 -> 0,
+                   CKMDelta -> 0,
+                   PMNSTheta12 -> 0,
+                   PMNSTheta13 -> 0,
+                   PMNSTheta23 -> 0,
+                   PMNSDelta -> 0,
+                   PMNSAlpha1 -> 0,
+                   PMNSAlpha2 -> 0,
+                   alphaEm0 -> 1/137.035999074,
+                   Mh -> 125.09
+               },
+               fsModelParameters -> {
+                   Ms -> MS,
+                   TanBeta -> 10,
+                   BMuInput -> MS^2,
+                   LamSDInput -> -0.5,
+                   LamSUInput -> -0.5,
+                   LamTDInput -> -0.5, 
+                   LamTUInput -> -0.5,
+                   MDBSInput -> MS,
+                   mRu2Input -> MS^2, 
+                   MuDInput -> -MS,
+                   MuUInput -> MS,
+                   MDWBTInput -> MS, 
+                   MDGocInput -> MS,
+                   mT2Input -> MS^2,
+                   mS2Input -> scaleFactor MS^2, 
+                   moc2Input -> scaleFactor MS^2,
+                   mq2Input -> scaleFactor MS^2 IdentityMatrix[3], 
+                   md2Input -> scaleFactor MS^2 IdentityMatrix[3], 
+                   mu2Input -> MS^2 IdentityMatrix[3],
+                   ml2Input -> MS^2 IdentityMatrix[3], 
+                   me2Input -> MS^2 IdentityMatrix[3], 
+                   mRd2Input -> MS^2
+               }
+           ];
+           spec = FSMRSSM2CalculateSpectrum[handle];
+           obs  = FSMRSSM2CalculateObservables[handle];
+           FSMRSSM2CloseHandle[handle];
+           If[spec === $Failed, Return[invalid]];
+           Join[MRSSM2 /. spec, MRSSM2 /. obs]
+          ];
+
+RunMRSSM[MS_] :=
+    Module[{spec = RunMRSSMBMP1[MS]},
+           If[spec === invalid,
+              Array[invalid&, {4}],
+              {
+                  FlexibleSUSYObservable`aMuon /. spec,
+                  (Pole[M[hh]] /. spec)[[1]],
+                  Pole[M[VWm]] /. spec,
+                  Pole[M[Glu]] /. spec
+              }
+             ]
+          ];
+
+(* Print[{ *)
+(*     N[#], Sequence @@ RunMRSSM[#] *)
+(* }]& /@ LogRange[200, 10000, 10]; *)
+
+data = ParallelMap[
+    { N[#], Sequence @@ RunMRSSM[#] }&,
+    LogRange[100, 10^4, 60]
+];
+
+Export["scan_MRSSM2_MS_amu_Mh_MW_MG.dat", data];
